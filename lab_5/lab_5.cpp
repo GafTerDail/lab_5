3ba1d70a24733beec00c36e81968d5217860e0d3
GafTerDail <81242177+GafTerDail@users.noreply.github.com>
2021-05-05 16:58:25 +0300

    Update lab_5.cpp

--- lab_5/lab_5.cpp
+++ lab_5/lab_5.cpp
@@ -4,23 +4,23 @@
 using namespace std;
 int main()
 {
-	ifstream f("C:\\ttt.txt");
-	ofstream f2("C:\\rrr.txt");
-	f.seekg(0, ios::end);
-	const int fsize = f.tellg();
-	f.seekg(0, ios::beg);
+	ifstream inf("C:\\ttt.txt");
+	ofstream outf("C:\\rrr.txt");
+	inf.seekg(0, ios::end);
+	const int fsize = inf.tellg();
+	inf.seekg(0, ios::beg);
 	char* buf = new char[fsize];
 	float* buf2 = new float[fsize];
-	f.read(buf, fsize);
+	inf.read(buf, fsize);
 	int i = 0;
 	for (i = 0; i < fsize; i++)
 	{
 		buf2[i] = 1 / ((float)buf[i]);
 		cout << buf2[i] << endl;
 	}
-	f2.write((char*)buf2, fsize);
-	f.close();
-	f2.close();
+	outf.write((char*)buf2, fsize);
+	inf.close();
+	outf.close();
 	delete[] buf;
 	delete[] buf2;
 	return 0;
