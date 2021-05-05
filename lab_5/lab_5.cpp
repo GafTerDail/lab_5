f8e54cd84a5f45ecbe54f5a84d3abac7be3bfeb4
Artem <acosta2002@mail.ru>
2021-05-05 16:47:17 +0300

    Добавьте файлы проекта.

---
+++ lab_5/lab_5.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int array1[10];
+    cout << "Enter elementi massiva: " << endl;
+    int sum = 0;
+    for (int counter = 0; counter < 10; counter++)
+        cin >> array1[counter];
+    cout << "oct" << "\t"
+        << "dec" << "\t"
+        << "hex" << "\t" << endl;
+    for (int counter = 0; counter < 10; counter++)
+        cout << oct << array1[counter] << "\t"
+        << dec << array1[counter] << "\t" << uppercase
+        << hex << array1[counter] << "\t" << endl;
+    system("pause");
+    return 0;
+};
